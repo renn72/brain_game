ad02883f2b8a127e9fdc86c2dfd510e9379e0f93
david <simplesam8856@gmail.com>
2021-07-22 17:13:10 +1000

    path fix

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,11 @@ app.use('/api/high_score', highScoreController)
 
 if (process.env.NODE_ENV === 'production') {
   const path = require('path')
-  app.use(express.static(path.join(__dirname, 'build')))
+  const __dirname = path.resolve()
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
   })
 }
 
